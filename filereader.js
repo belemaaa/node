0b2465792b9cc1ed078510444511ea23455c333b
belemaaa <graceitamunoala@gmail.com>
2023-12-23 15:13:01 +0000

    using promises class for file operations

--- filereader.js
+++ filereader.js
@@ -1,22 +1,43 @@
 const fs = require('fs')
+const fsPromises = require('fs').promises
 const path = require('path')
 
-fs.readFile(path.join(__dirname, 'files', 'testfile.txt'), 'utf-8', (err, data) => {
-    if (err) throw err
-    console.log(data)
-})
 
-const write_data = "This is a write operation task"
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), write_data, (err) => {
-    if (err) throw err
-    console.log('Write operation complete')
-})
+const fileops = async () => {
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'testfile.txt'), 'utf-8')
+        console.log(data)
+        const write_data = await fsPromises.writeFile(path.join(__dirname, 'files', 'newreply.txt'), "Some content")
+        console.log('Write operation complete')
+    } catch (err){
+        console.error(`Errors retrieved: ${err}`)
+    }
+}
 
-const append_data = "\nAppending content to file"
-fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), append_data, (err) => {
-    if (err) throw err
-    console.log('Append operation complete')
-})
+fileops()
+
+// fs.readFile(path.join(__dirname, 'files', 'testfile.txt'), 'utf-8', (err, data) => {
+//     if (err) throw err
+//     console.log(data)
+// })
+
+// const write_data = "This is a write operation task"
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), write_data, (err) => {
+//     if (err) throw err
+//     console.log('Write operation complete')
+
+//     const append_data = "\nAppending content to file"
+//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), append_data, (err) => {
+//         if (err) throw err
+//         console.log('Append operation complete')
+
+//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), (path.join(__dirname, 'files', 'newreply.txt')), (err) => {
+//             if (err) throw err
+//             console.log('rename operation complete')
+//         })
+//     })
+// })
+
 
 
 // exit on uncaught errors
