d5f1a350a311642a8e11dd5ac5d30d397458728b
belemaaa <graceitamunoala@gmail.com>
2023-12-28 20:12:39 +0000

    modularity

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const product_routes = require('./api/routes/products')
 const order_routes = require('./api/routes/orders')
 
 // connect mongodb database
-const db = (module.exports = () => {
+const database = () => {
     try{
         mongoose.connect('mongodb+srv://graceitamunoala:'+process.env.MONGO_ATLAS_PW+'@node-proj.w2tuwms.mongodb.net/?retryWrites=true&w=majority', {
             useNewUrlParser: true,
@@ -22,42 +22,59 @@ const db = (module.exports = () => {
         console.log(error)
         console.log('Database connection failed')
     }
-})
-db();
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false})) 
-app.use(bodyParser.json())
+}
 
-app.use((req, res, next) => {
-    // allow cors for all ports/servers
-    res.header('Access-Control-Allow-Origin', '*') 
-    res.header(
-        'Access-Control-Allow-Headers', 
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-    if (req.method === 'OPTIONS'){
-        res.headersSent('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET, PATCH')
-        return res.status(200).json({})
-    }
-    next()
-})
+const middlewares = () => {
+    app.use(morgan('dev'))
+    app.use(bodyParser.urlencoded({extended: false})) 
+    app.use(bodyParser.json())
+}
 
-app.use('/products', product_routes)
-app.use('/orders', order_routes)
-
-app.use((req, res, next) => {
-    const error = new Error('Not found')
-    error.status = 404
-    next(error)
-})
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
+const headers = () => {
+    app.use((req, res, next) => {
+        // allow cors for all ports/servers
+        res.header('Access-Control-Allow-Origin', '*') 
+        res.header(
+            'Access-Control-Allow-Headers', 
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        )
+        if (req.method === 'OPTIONS'){
+            res.headersSent('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET, PATCH')
+            return res.status(200).json({})
         }
+        next()
     })
-})
+}
+
+const routes = ()=> {
+    app.use('/products', product_routes)
+    app.use('/orders', order_routes)
+}
+
+const not_found_error = () => {
+    app.use((req, res, next) => {
+        const error = new Error('Not found')
+        error.status = 404
+        next(error)
+    })
+}
+
+const handle_errors = () => {
+    app.use((error, req, res, next) => {
+        res.status(error.status || 500)
+        res.json({
+            error: {
+                message: error.message
+            }
+        })
+    })
+}
+
+database()
+middlewares()
+headers()
+routes()
+not_found_error()
+handle_errors()
 
 module.exports = app
