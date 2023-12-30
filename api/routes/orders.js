ce008f5b70dbc417715ce22b6e948fa16e0bf6c6
belemaaa <graceitamunoala@gmail.com>
2023-12-30 18:06:00 +0000

    testing for non existng products before handling order creation

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -24,46 +24,42 @@ const create_order = () => {
         const quantity = req.body.quantity || 1
         try{
             const existing_product = await Product.findById(product_id).exec()
-            if (existing_product){
-                const product_price = parseFloat(existing_product.price)
-                const calculated_price = parseFloat(product_price * quantity)
-                console.log(product_price, calculated_price)
-                const order = new Order({
-                    _id: new mongoose.Types.ObjectId(),
-                    product: product_id,
-                    quantity: quantity,
-                    price: calculated_price
-                })
-                const saved_order = await order.save()
-                const response = {
-                    order_id: saved_order._id,
-                    product_id: saved_order.product,
-                    quantity: saved_order.quantity,
-                    price: saved_order.price,
-                    request: {
-                        type: 'GET',
-                        description: 'GET_PRODUCT_DETAIL',
-                        url: 'http://localhost:3000/products/' + product_id
-                    }
-                }
-                res.status(201).json({
-                    message: "New order created for product " + product_id,
-                    response
-                })
-            }else{
+            if (!existing_product){
                 res.status(404).json({
                     message: "Invalid product id"
                 })
             }
+            const product_price = parseFloat(existing_product.price)
+            const calculated_price = parseFloat(product_price * quantity)
+            const order = new Order({
+                _id: new mongoose.Types.ObjectId(),
+                product: product_id,
+                quantity: quantity,
+                price: calculated_price
+            })
+            const saved_order = await order.save()
+            const response = {
+                order_id: saved_order._id,
+                product_id: saved_order.product,
+                quantity: saved_order.quantity,
+                price: saved_order.price,
+                request: {
+                    type: 'GET',
+                    description: 'GET_PRODUCT_DETAIL',
+                    url: 'http://localhost:3000/products/' + product_id
+                }
+            }
+            res.status(201).json({
+                message: "New order created for product " + product_id,
+                response
+            })    
         }catch(error){
             console.log(error)
             res.status(500).json({
                 error: error,
             })
-        }
-        
-    })
-    
+        } 
+    })  
 }
 
 get_orders()
